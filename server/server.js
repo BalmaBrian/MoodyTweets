9878c1321cfe9daa2f7b62e18dd2c092b7454d31
jeckoy29 <49110177+jeckoy29@users.noreply.github.com>
2020-02-22 22:53:15 -0800

    Added Happy HTML Rough Draft

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-const express = require('express');
+//const express = require('express');
 const path = require('path');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../public'), {index: false}))
+
 
 app.listen("4200", function() { 
     console.log("Server running on 4200")
