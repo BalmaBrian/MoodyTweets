97288a02d3dba450ca533ad778d77d09ed21f2fe
artem-tkachuk <artemtkachuk@yahoo.com>
2020-02-23 01:17:53 -0800

    First server commit. Going to merge public to the server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,31 @@
-//const express = require('express');
-const path = require('path');
-const axios = require('axios');
-const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../public'), {index: false}))
+app.get('/', (req, res) => {
+    res.send('Hello from App Engine!');
+});
 
+app.get('/submit-username', (req, res) => {
+    const handle = req.body.handle;
 
-app.listen("4200", function() { 
-    console.log("Server running on 4200")
-})
+    // let tweets = await callGetTweets(handle);
+
+    //let mood = await ML(tweets);  //1...10
+
+    // switch(mood) {
+    //     case 1:
+    //         render('sad');
+    //     case 2;
+    //         render('normal');
+    //         .....
+    // }
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
---
+++ tweets/user_timeline.py
@@ -0,0 +1,132 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus.reader import wordnet
+import nltk
+import re
+import pandas as pd
+import os
+
+from tweepy import OAuthHandler
+from tweepy import API
+
+
+def main_func(person:str):
+    class CleanText:
+
+        @staticmethod
+        def remove_stopwords(string: str):
+            """
+            Removes all the stop words in a string
+            :param string: the string
+            :return: a new containing no stop words
+            """
+
+            stop_words = set(stopwords.words('english'))
+            return ' '.join([word for word in string.split() if word not in stop_words])
+
+        @staticmethod
+        def remove_non_letters(string: str):
+            """
+            Removes non letters from string.
+            :param string: The string.
+            :return: A string with only lowercase letters.
+            """
+
+            # Remove non_letters, separate by space, the string, lowercase
+            new_string = re.sub('[^a-zA-Z]', ' ', string).lower()
+            # To list, to string
+            # To remove whitespace
+            string = ' '.join(new_string.split())
+            return string.replace('\n', '')
+
+        @staticmethod
+        def stem_string(string: str):
+            stemmer = PorterStemmer()
+            return ' '.join([stemmer.stem(word=word) for word in string.split()])
+
+        @staticmethod
+        def wordnet_lemmatizer(string: str):
+            tag_dict = {'J': wordnet.ADJ, 'N': wordnet.NOUN, 'V': wordnet.VERB, 'R': wordnet.ADV}
+            lemmatizer = WordNetLemmatizer()
+            new_string = []
+            for word in string.split():
+                tag = nltk.pos_tag([word])[0][1][0].upper()
+                if tag in tag_dict:
+                    word = lemmatizer.lemmatize(word, pos=tag_dict[tag])
+                else:
+                    word = lemmatizer.lemmatize(word, pos=wordnet.NOUN)
+
+                new_string.append(word)
+            return ' '.join(new_string)
+            # return ' '.join([lemmatizer.lemmatize(word) for word in string.split()])
+
+        @staticmethod
+        def remove_links(string: str):
+            """
+            Removes the links in the string
+            :param string: The new string.
+            :return: A string.
+            """
+
+            return ' '.join([s for s in string.split() if 'http' not in s])
+
+        @staticmethod
+        def remove_rp_text(string: str):
+            """
+            Removes the 'RP @' text
+            :param string:
+            :return:
+            """
+
+            new_string = []
+            for str_ in string.split():
+                if str_.startswith('RP @'):
+                    str_ = str_[4:]
+                    new_string.append(str_)
+                elif str_.startswith('rt '):
+                    str_ = str_[4:]
+                    new_string.append(str_)
+                else:
+                    pass
+
+        @staticmethod
+        def clean_string(string: str):
+            # text = CleanText.remove_links(string)
+            text = CleanText.remove_non_letters(string)
+            # text = CleanText.remove_stopwords(text)
+            # text = CleanText.wordnet_lemmatizer(text)
+            return text
+
+
+    # KEYS
+    TWITTER_API_KEY = 'NmKUsZ4A6OLttHgIclyxIlEhV'
+    TWITTER_API_SECRET_KEY = '7wYalNckdjwESXa1L0ijMeLHOaLjNQKSt5QuVfqEqJh6m9FUvT'
+    TWITTER_ACCESS_TOKEN = '1089278284318142464-SQhuvhyAHkvM8IvYl0Qn5kUrQM6N5r'
+    TWITTER_ACCESS_TOKEN_SECRET = 'cKfDtn6qKcmQkOp5CcWWgjNaWNh1TRpwS3T73OSNwH109'
+
+    # INIT
+    auth = OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET_KEY)
+    auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+    auth_api = API(auth)
+
+    # GETS THE FINAL DATA
+    tweets = auth_api.user_timeline(screen_name=person, count=500, tweet_mode="extended")
+    cleaned_tweets = []
+    cleaner = CleanText()
+    for tweet in tweets:
+        tweet = tweet.full_text
+        tweet = cleaner.clean_string(tweet)
+        # if not tweet.startswith('RT @'):
+        #     cleaned_tweets.append(tweet)
+        cleaned_tweets.append(tweet)
+
+    dataset = pd.DataFrame(data=cleaned_tweets, columns=None, index=None)
+
+    dataset.to_csv(r'dataset1.txt')
+    print(dataset.dtypes)
+
+main_func('realdonaldtrump')
+
+
